96f7ca03a9c27cedc5feef5209e83b145e471c91
Deon Moolman <cmdrdats@gmail.com>
2012-12-30 19:10:32 +0200

    Set the version to correspond with the cljminecraft version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljminecraft/lein-template "0.1.0-SNAPSHOT"
+(defproject cljminecraft/lein-template "1.0.2"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
--- src/leiningen/new/cljminecraft/project.clj
+++ src/leiningen/new/cljminecraft/project.clj
@@ -1,13 +1,14 @@
 (defproject {{name}} "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[cljminecraft "1.0.1-SNAPSHOT"]
+  :dependencies [[cljminecraft "1.0.2"]
                      ;make sure any required projects here either are already in clj-minecraft uberjar or
                      ;just make this an uberjar; or find a way to add them to ../lib in bukkit
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [org.bukkit/bukkit "1.4.5-R0.3-SNAPSHOT"]]
+                 [org.bukkit/bukkit "1.4.5-R1.0"]]
   :repl-options [:init nil :caught clj-stacktrace.repl/pst+]
   :repositories [;"spout-repo-snap" "http://repo.getspout.org/content/repositories/snapshots/"
                  ;"spout-repo-rel" "http://repo.getspout.org/content/repositories/releases/"
                  ;"org.bukkit" "Bukkit" "bukkit" {:url "http://repo.bukkit.org/content/groups/public/"}
-                 ["bukkit.snapshots" "http://repo.bukkit.org/content/repositories/snapshots"]])
+                 ["bukkit.snapshots" "http://repo.bukkit.org/content/repositories/snapshots"]
+                 ["bukkit.releases" "http://repo.bukkit.org/content/repositories/releases"]])
